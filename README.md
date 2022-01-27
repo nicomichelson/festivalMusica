11532e53d3db4b21b17ae88634cdac547b056059
nicomichelson <nicomichelson@gmail.com>
2022-01-27 11:57:50 -0300

    ..

---
+++ README.md
@@ -0,0 +1,6 @@
+## FESTIVAL DE MUSICA
+GULP CSS HTML
+
+OPTIMICACION DE IMAGENES
+https://github.com/imagemin/imagemin-webp#imageminwebpoptions
+https://github.com/sindresorhus/gulp-webp
--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,51 @@
-const { src, dest, watch } = require('gulp'); 
+const { src, dest, watch, parallel } = require('gulp');
+
+//css
 const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber');
+
+//imagenes
+const imagemin = require('gulp-imagemin');
+const cache = require('gulp-cache');
+const webp = require('gulp-webp');
 
 function css(done){
    
-    src('src/scss/app.scss') //Identificar el archivo .SCSS a compilar
+    src('src/scss/**/*.scss') //Identificar el archivo .SCSS a compilar
+        .pipe(plumber()) // no detiene el workflow en caso de q tranq algo
         .pipe(sass()) //Compilar
         .pipe(dest('build/css')) // Almacenar en disco
+    done();
+}
+function imagenes(done){
+    const opciones = {
+        optimizationLevel: 3
+    }
+    src('src/img/**/*.{png,jpg}')
+        .pipe(cache(imagemin(opciones)))
+        .pipe(dest('build/img'))
+    done();
+}
+
+function versionWebp( done ) {
+    const opciones = {
+        quality: 50
+    };
+    src('src/imagen/**/*.{png,jpg}')
+        .pipe( webp(opciones) )
+        .pipe( dest('build/img') )
     done();
 }
 
 function dev(done){
     //identifica el archivo y q tarea manda a llamar para un cambio
     //sin tener q compilar
-    watch('src/scss/app.scss', css);
+    watch('src/scss/**/*.scss', css);
     done();
 }
 
-function tarea(done){
-    console.log('Desde la primer tarea');
-    done();
-}
 
-exports.tarea = tarea;
 exports.css = css;
-exports.dev = dev;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.dev = parallel(versionWebp,dev);
